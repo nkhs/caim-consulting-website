8db6320f5f9ed1b4ef2f0d8450197b2c6b6dfeff
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-07-28 01:37:34 +0530

    feat: add new JS for message box

---
+++ mainsite/static/js/messageBox.js
@@ -0,0 +1,67 @@
+jQuery(document).ready(function ($) {
+  "use strict";
+
+  // CSRF TOKEN FOR AJAX
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+  const csrftoken = getCookie("csrftoken");
+
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
+  }
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    },
+  });
+
+  // Scroll to the input box
+  $("html, body").animate(
+    {
+      scrollTop: $("#chatBox")[0].scrollHeight,
+    },
+    1000
+  );
+
+  // Message Box
+  $("form.messageBox").submit(function () {
+    console.log(this);
+    var action = $(this).attr("action");
+    var id = $(this).attr("id");
+    console.log(id);
+    if (!action) {
+      console.log("performed action");
+      action = "/messageinput/";
+    }
+    var userInput = $("#message").val();
+    console.log(userInput);
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: {
+        message: userInput,
+        chat: id,
+      },
+      success: function (data) {
+        window.location.href = "";
+      },
+    });
+    return false;
+  });
+});
