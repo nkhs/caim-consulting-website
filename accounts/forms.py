710b382bf636b9983d02d11d75eb4a0b775355bb
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-08-06 03:11:04 +0530

    feat: use bootstrap for sign up form

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,6 +13,16 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
 
+    username.widget.attrs.update({"class": "form-control"})
+    email.widget.attrs.update({"class": "form-control"})
+    first_name.widget.attrs.update({"class": "form-control"})
+    last_name.widget.attrs.update({"class": "form-control"})
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].widget.attrs.update({"class": "form-control"})
+        self.fields["password2"].widget.attrs.update({"class": "form-control"})
+
     class Meta:
         model = User
         fields = (
