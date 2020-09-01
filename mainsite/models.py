849ad91bef8bcec94c866ff897bc301d4e9dd0e4
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-09-01 18:09:47 +0530

    fix: add unique check to Service Name

--- mainsite/models.py
+++ mainsite/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Service(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
