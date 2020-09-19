04c46cf2d2e201723c8c99508526cbed39050c0f
chttrjeankr <chttrjeankr@gmail.com>
2020-09-20 00:28:01 +0530

    feat: add services page
    * view, url, template
    * Know More button

--- mainsite/views.py
+++ mainsite/views.py
@@ -18,6 +18,10 @@ def disclaimer(request):
     return render(request, "disclaimer.html")
 
 
+def services(request):
+    return render(request, "services.html")
+
+
 def advisory_board(request, member):
     advisor = get_object_or_404(Advisor, slug=member)
     return render(request, "advisor_bio.html", {"advisor": advisor})
