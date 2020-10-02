8360f41cc44fd7c1e1ade0b915bea2db84476e3d
chttrjeankr <chttrjeankr@gmail.com>
2020-10-02 12:32:55 +0530

    refactor: prettier format
    [HUGE COMMIT]

--- accounts/static/js/toggleChatbox.js
+++ accounts/static/js/toggleChatbox.js
@@ -1,14 +1,14 @@
 function toggleChatStartup() {
   var chatbox = document.getElementById("chatstart-box");
   var toggleBtn = document.getElementById("toggle-chatbox-btn");
-  var graphicProfile = document.getElementById("graphic-profile")
+  var graphicProfile = document.getElementById("graphic-profile");
   if (chatbox.style.display == "block") {
     chatbox.style.display = "none";
     graphicProfile.style.display = "block";
     toggleBtn.innerHTML = "Apply for Assistance";
   } else {
     chatbox.style.display = "block";
-    graphicProfile.style.display = "none"
+    graphicProfile.style.display = "none";
     toggleBtn.innerHTML = "Close Application";
   }
 }
